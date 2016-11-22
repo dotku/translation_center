6e3dd208b84202048b071b2ed148482ca153f543
EC2 Default User <you@example.com>
2016-11-22 21:35:00 +0000

    first commit

---
+++ index.php
@@ -0,0 +1,7 @@
+<?php 
+define('APP_PATH', 'app/');
+define('RUNTIME_PATH', 'runtime/');
+define('COMMON_PATH', 'common/');
+define('APP_DEBUG', true);
+
+require_once '/var/www/vendor/topthink/thinkphp/ThinkPHP/ThinkPHP.php';
